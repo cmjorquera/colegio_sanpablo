837d46ed91e80e9e6a5290ae13a9cae9b6d69f28
Cristian Jorquera <cm.jorquerag@gmail.com>
2026-04-19 21:44:20 -0400

    cambio

--- componentes/topbar.php
+++ componentes/topbar.php
@@ -21,9 +21,9 @@ $direccion = $institution['direccion'] ?? '';
                 <?php endif; ?>
             </div>
             <div class="d-flex align-items-center gap-3">
-                <a href="#configuracion" class="sp-login-btn" title="Configuración institucional">
-                    <i class="fas fa-cog"></i>
-                    <span>Institución</span>
+                <a href="#" class="sp-login-btn" data-bs-toggle="modal" data-bs-target="#modalLogin" title="Ingresar al sistema">
+                    <i class="fas fa-sign-in-alt"></i>
+                    <span>Ingresar</span>
                 </a>
                 <?php if (!empty($institution['instagram'])): ?>
                     <span class="sep">|</span>
@@ -32,6 +32,58 @@ $direccion = $institution['direccion'] ?? '';
                 <?php if (!empty($institution['facebook'])): ?>
                     <a href="<?= e($institution['facebook']) ?>" target="_blank" rel="noopener"><i class="fab fa-facebook me-1"></i>Facebook</a>
                 <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modalLogin" tabindex="-1" aria-labelledby="modalLoginLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content sp-modal-login">
+            <div class="sp-modal-colorband"></div>
+            <div class="modal-body p-0">
+                <div class="sp-modal-header">
+                    <button type="button" class="sp-modal-close" data-bs-dismiss="modal" aria-label="Cerrar">
+                        <i class="fas fa-times"></i>
+                    </button>
+                    <div class="sp-modal-logo">
+                        <i class="fas fa-school"></i>
+                    </div>
+                    <h5 id="modalLoginLabel">Mi Colegio San Pablo</h5>
+                    <p>Elige el área a la que deseas ingresar:</p>
+                </div>
+
+                <div class="sp-modal-areas">
+                    <a href="admin.php" class="sp-area-btn sp-area-admin">
+                        <i class="fas fa-lock"></i>
+                        <span>Administrador</span>
+                        <i class="fas fa-chevron-right sp-area-arrow"></i>
+                    </a>
+
+                    <a href="#" class="sp-area-btn" data-bs-dismiss="modal">
+                        <i class="fas fa-user-graduate"></i>
+                        <span>Área Alumnos</span>
+                        <i class="fas fa-chevron-right sp-area-arrow"></i>
+                    </a>
+
+                    <a href="#" class="sp-area-btn" data-bs-dismiss="modal">
+                        <i class="fas fa-users"></i>
+                        <span>Área Padres</span>
+                        <i class="fas fa-chevron-right sp-area-arrow"></i>
+                    </a>
+
+                    <a href="#" class="sp-area-btn" data-bs-dismiss="modal">
+                        <i class="fas fa-briefcase"></i>
+                        <span>Área Funcionario</span>
+                        <i class="fas fa-chevron-right sp-area-arrow"></i>
+                    </a>
+
+                    <a href="#" class="sp-area-btn" data-bs-dismiss="modal">
+                        <i class="fas fa-chalkboard-teacher"></i>
+                        <span>Área Docentes</span>
+                        <i class="fas fa-chevron-right sp-area-arrow"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </div>

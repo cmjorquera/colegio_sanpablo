ae91af4603e4329002f74fae971af806755f49e4
Cristian Jorquera <cm.jorquerag@gmail.com>
2026-04-19 22:04:40 -0400

    cambio

--- componentes/header_principal.php
+++ componentes/header_principal.php
@@ -14,10 +14,10 @@ $urlBoton = $institution['url_boton_principal'] ?? '#';
                         <img src="<?= e($logoHeader) ?>" alt="<?= e($nombreInstitucion) ?>" onerror="this.src='assets/images/logo/logo.svg'">
                     </a>
                 </div>
-                <div class="d-none d-lg-block">
+                <!-- <div class="d-none d-lg-block">
                     <div class="fw-semibold" style="color:var(--sp-azul)"><?= e($nombreInstitucion) ?></div>
                     <?php if ($dominio !== ''): ?><small class="text-muted"><?= e($dominio) ?></small><?php endif; ?>
-                </div>
+                </div> -->
             </div>
 
             <div class="d-flex align-items-center gap-4 ms-auto">
